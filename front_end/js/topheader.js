fa6be3c1d86bd2049e237856c0281ce1d6213b7f
emiliano <pedica.1849824@studenti.uniroma1.it>
2021-05-04 00:11:00 +0200

    added simple loginButton

--- front_end/js/topheader.js
+++ front_end/js/topheader.js
@@ -3,10 +3,12 @@ Vue.component('topheader', {
         <header>
             <div class="container">
                 <a href="https://www.uniroma1.it/it/">
-                    <img class="float-start" style="margin-top: 20px; margin-bottom: 10px;" src="/front_end/assets/img/logo_full.png" alt="Uniroma 1">
+                    <img class="float-start" style="margin-top: 20px; margin-bottom: 10px;" src="../assets/img/logo_full.png" alt="Uniroma 1">
                 </a>
                 <h1 class="float-end" style="margin-top: 20px;">
                     <a class="title" href="/front_end/index.html" title="Home page">CUR - Centro Unico di Ricerca Sapienza</a>
+                    <button class="accountButton" onclick="window.location.href='../login/index.html'" type="button" class="btn btn-secondary" > Entra      </button>
+                    <button class="accountButton" onclick="window.location.href='../signup/index.html'" type="button" class="btn btn-secondary"> Registrati </button>
                 </h1>
             </div>
 
@@ -18,6 +20,7 @@ Vue.component('topheader', {
                 <a href="javascript:void(0);" class="icon" onclick="doHamburger()">
                 <i class="fa fa-bars"></i>
                 </a>
+                
             </div>
         </header>
         `
