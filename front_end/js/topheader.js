c324dfef1e3f735f18454a786b0d27607a0d7325
lorenzosoannini <lorenzosoannini@hotmail.it>
2021-05-04 18:40:46 +0200

    Adjusted relative paths with root paths in topheader

--- front_end/js/topheader.js
+++ front_end/js/topheader.js
@@ -3,12 +3,12 @@ Vue.component('topheader', {
         <header>
             <div class="container">
                 <a href="https://www.uniroma1.it/it/">
-                    <img class="float-start" style="margin-top: 20px; margin-bottom: 10px;" src="../assets/img/logo_full.png" alt="Uniroma 1">
+                    <img class="float-start" style="margin-top: 20px; margin-bottom: 10px;" src="/front_end/assets/img/logo_full.png" alt="Uniroma 1">
                 </a>
                 <h1 class="float-end" style="margin-top: 20px;">
                     <a class="title" href="/front_end/index.html" title="Home page">CUR - Centro Unico di Ricerca Sapienza</a>
-                    <button class="accountButton" onclick="window.location.href='../login/index.html'" type="button" class="btn btn-secondary" > Entra      </button>
-                    <button class="accountButton" onclick="window.location.href='../signup/index.html'" type="button" class="btn btn-secondary"> Registrati </button>
+                    <button class="accountButton" onclick="window.location.href='/front_end/login/index.html'" type="button" class="btn btn-secondary" > Entra      </button>
+                    <button class="accountButton" onclick="window.location.href='/front_end/signup/index.html'" type="button" class="btn btn-secondary"> Registrati </button>
                 </h1>
             </div>
 
@@ -19,8 +19,7 @@ Vue.component('topheader', {
                 <a href="#">Contatti</a>
                 <a href="javascript:void(0);" class="icon" onclick="doHamburger()">
                 <i class="fa fa-bars"></i>
-                </a>
-                
+                </a>                
             </div>
         </header>
         `
