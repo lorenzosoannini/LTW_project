73fadbb1fad542cfce929a1d3eaa99b8ab6a16cc
lorenzosoannini <lorenzosoannini@hotmail.it>
2021-05-14 20:40:31 +0200

    New login error ui

--- front_end/js/topheader.js
+++ front_end/js/topheader.js
@@ -13,8 +13,8 @@ Vue.component('topheader', {
                             <a class="title" href="/front_end/index.html" title="Home page">CURS - Centro Unico di Ricerca Sapienza</a>
                         </h1>
                         <div class="btn-group float-end mb-2" role="group">
-                            <a class="btn btn-danger" href="/front_end/login/" role="button" id="loginButton">Log-In</a>
-                            <a class="btn btn-danger" href="/front_end/signup/" role="button" id="signupButton">Registrati</a>
+                            <a class="btn btn-outline-danger" href="/front_end/login/" role="button" id="loginButton">Log-In</a>
+                            <a class="btn btn-outline-danger" href="/front_end/signup/" role="button" id="signupButton">Registrati</a>
                         </div>
                     </div>
                 </div>
