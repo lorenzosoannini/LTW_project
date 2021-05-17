c86d798d0ce81b121db1d9d70a1ee7b931b44347
unknown <emiliano4458@gmail.com>
2021-05-17 19:51:52 +0200

    added logout and ricercatori

--- front_end/js/check_cookies.js
+++ front_end/js/check_cookies.js
@@ -1,25 +1,12 @@
-<<<<<<< HEAD
 
-var username = getCookie("username");
-if (username != "") {
-    document.getElementById("logInButton").style="display: none";
-    document.getElementById("d_down").style="display: none";
-    document.getElementById("signUpButton").style="display: none";
-    document.getElementById("alreadyLogged").innerHTML=getCookie("username");
-    document.getElementById("form_login").style="visibility: hidden;";
-    document.getElementById("line_").innerHTML="Puoi continuare a navigare nel sito";
-    document.getElementById("login_effettuato").innerHTML="Ciao "+getCookie("username")+", hai già effettuato l accesso";
-      }
-=======
 document.addEventListener('DOMContentLoaded', function() {
     var username = getCookie("username");
     if (username != "") {
         document.getElementById("loginButton").style="display: none";
         document.getElementById("signupButton").style="display: none";
-        document.getElementById("alreadyLogged").innerHTML="Bentornato <b>"+getCookie("username")+"</b>";
-        document.getElementById("alreadyLogged").hidden = false;
-    }
->>>>>>> e9bbcaef53c69176aa6321d88b22855368d6185d
+        document.getElementById("d_down").style="display: block";
+        document.getElementById("dropdownMenuButton1").innerHTML="Bentornato <b>"+getCookie("username")+"</b>"
+        }
 
 
     function getCookie(cname) {
@@ -47,4 +34,11 @@ document.addEventListener('DOMContentLoaded', function() {
             return true; 
         }
     }
+
+
 });
+
+function deleteCookie(){
+    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    window.location.reload(true);
+}
