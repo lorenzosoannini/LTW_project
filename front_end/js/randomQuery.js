f353850932642e0128a1b2bee66b167cfe8458cf
lorenzosoannini <lorenzosoannini@hotmail.it>
2021-05-24 15:22:38 +0200

    Fixed random page button firing only odds clicks

--- front_end/js/randomQuery.js
+++ front_end/js/randomQuery.js
@@ -1,20 +1,27 @@
 function loadNewQuery(){
-    document.getElementById("descrizione").innerHTML="";
-    function reqListener () {
-    console.log(this.responseText);
+    if(document.getElementById("casual_page").getAttribute("odd") == 'true'){
+        document.getElementById("casual_page").setAttribute("odd", 'false');
+
+        document.getElementById("descrizione").innerHTML="";
+        function reqListener () {
+        console.log(this.responseText);
+        }
+        var oReq = new XMLHttpRequest();
+        oReq.onload = function() {
+            var testo=this.responseText;
+            var testo2=testo.replace(/\"/g,'§');
+            var testo3=testo2.split("§"); //array
+            document.getElementById("titolo").innerHTML=testo3[1];
+            document.getElementById("autore").innerHTML=testo3[3];
+            document.getElementById("descrizione").innerHTML=testo3[7];
+            //document.getElementById("titolo_").innerHTML=testo3[1];
+        };
+        oReq.open("get", "js/random_page.php", true);                       
+        oReq.send();
     }
-    var oReq = new XMLHttpRequest();
-    oReq.onload = function() {
-        var testo=this.responseText;
-        var testo2=testo.replace(/\"/g,'§');
-        var testo3=testo2.split("§"); //array
-        document.getElementById("titolo").innerHTML=testo3[1];
-        document.getElementById("autore").innerHTML=testo3[3];
-        document.getElementById("descrizione").innerHTML=testo3[7];
-        //document.getElementById("titolo_").innerHTML=testo3[1];
-    };
-    oReq.open("get", "js/random_page.php", true);                       
-    oReq.send();
+    else{
+        document.getElementById("casual_page").setAttribute("odd", 'true');
+    }
 }
 var oReq = new XMLHttpRequest();
 
